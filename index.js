448f2149c8668946c552955c1ba5038f14739f8f
Zlomorda <61455902+Zlomorda@users.noreply.github.com>
2020-12-20 01:35:23 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,24 +3,30 @@ const fetch = require('node-fetch')
 const app = express()
 const path = require('path')
 
-app.set('view wngine', 'ejs')
+app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
 
 app.get('/', (req, res) => {
-    const hello = async () => {
-        const responce = await fetch('https://type.fit/api/quotes')
-        const data = await responce.json()
-        const quoteData = {
-            text: data[5].text,
-            author: data[5].author,
-        }
-        return quoteData
-    }
-
-    hello().then((quote) => console.log(quote.text, quote.author))
-    // res.render('home.ejs')
+    res.render('home.ejs')
 })
 
+app.get('/cats', (req, res) => {
+    const cats = [
+        'cat 1',
+        'cat 2',
+        'catdfhdfh',
+        'cat 4',
+        'cat 5',
+        'cat 6',
+        'cat 7',
+    ]
+    res.render('cats.ejs', { cats })
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params
+    res.render('subreddit.ejs', { subreddit })
+})
 app.get('/rand', (req, res) => {
     const random = Math.floor(Math.random() * 10) + 1
     res.render('random.ejs', { random: random })
@@ -29,3 +35,15 @@ app.get('/rand', (req, res) => {
 app.listen(3000, () => {
     console.log('Listening on port 3k')
 })
+
+// const hello = async () => {
+//     const responce = await fetch('https://type.fit/api/quotes')
+//     const data = await responce.json()
+//     const quoteData = {
+//         text: data[5].text,
+//         author: data[5].author,
+//     }
+//     return quoteData
+// }
+
+// hello().then((quote) => console.log(quote.text, quote.author))
